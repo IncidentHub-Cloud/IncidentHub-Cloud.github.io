1e87f23b51be3f8c9a8d68d6c5ef71e8eb2701ea
Hrishikesh Barua <talonx@gmail.com>
2024-06-10 13:04:02 +0530

    Added robots.txt and sitemap

--- blog/docusaurus.config.ts
+++ blog/docusaurus.config.ts
@@ -34,6 +34,18 @@ const config: Config = {
     [
       'classic',
       {
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+        },        
         blog: {
           showReadingTime: true,
           routeBasePath: '/',
